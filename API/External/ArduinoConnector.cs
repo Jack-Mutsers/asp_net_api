62942186283a2252acda784e15a6f675a1521e7c
jack mutsers <jmutsers95@hotmail.com>
2020-01-16 14:35:12 +0100

    fixed arduino call issue

--- API/External/ArduinoConnector.cs
+++ API/External/ArduinoConnector.cs
@@ -15,71 +15,78 @@ namespace API.External
         const string SERVER_IP = "192.168.43.210";
         public bool Send_Data(Component comp, Category cat)
         {
-            string ip_Adress = "192.168.43.210";//comp.ip_adress;
-
-            //---data to send to the server---
-            string textToSend = "";
-
-            if (cat.name == "Lamp")
+            try
             {
-                switch (comp.value)
-                {
-                    case 1:
-                        textToSend = "LED-OFF";
-                        break;
-                    case 0:
-                        textToSend = "LED-ON";
-                        break;
-                }
-            }
-            else if (cat.name == "Deur")
-            {
-                switch (comp.value)
-                {
-                    case 1:
-                        textToSend = "DOOR-CLOSE";
-                        break;
-                    case 0:
-                        textToSend = "DOOR-OPEN";
-                        break;
-                }
-            }
-            else if (cat.name == "Lock")
-            {
-                switch (comp.value)
-                {
-                    case 1:
-                        textToSend = "LOCK-CLOSE";
-                        break;
-                    case 0:
-                        textToSend = "LOCK-OPEN";
-                        break;
-                }
-            }
-            else
-            {
-                switch (comp.value)
-                {
-                    case 1:
-                        textToSend = "WINDOW-CLOSE";
-                        break;
-                    case 0:
-                        textToSend = "WINDOW-OPEN";
-                        break;
-                }
-            }
+                string ip_Adress = "192.168.43.210";//comp.ip_adress;
 
-            //---create a TCPClient object at the IP and port no.---
-            TcpClient client = new TcpClient(ip_Adress, PORT_NO);
-            NetworkStream nwStream = client.GetStream();
-            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                //---data to send to the server---
+                string textToSend = "";
 
-            //---send the text---
-            Console.WriteLine("Sending : " + textToSend);
-            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                if (cat.name == "Lamp")
+                {
+                    switch (comp.value)
+                    {
+                        case 1:
+                            textToSend = "LED-OFF";
+                            break;
+                        case 0:
+                            textToSend = "LED-ON";
+                            break;
+                    }
+                }
+                else if (cat.name == "Deur")
+                {
+                    switch (comp.value)
+                    {
+                        case 1:
+                            textToSend = "DOOR-CLOSE";
+                            break;
+                        case 0:
+                            textToSend = "DOOR-OPEN";
+                            break;
+                    }
+                }
+                else if (cat.name == "Lock")
+                {
+                    switch (comp.value)
+                    {
+                        case 1:
+                            textToSend = "LOCK-CLOSE";
+                            break;
+                        case 0:
+                            textToSend = "LOCK-OPEN";
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (comp.value)
+                    {
+                        case 1:
+                            textToSend = "WINDOW-CLOSE";
+                            break;
+                        case 0:
+                            textToSend = "WINDOW-OPEN";
+                            break;
+                    }
+                }
 
-            client.Close();
-            return true;
+                //---create a TCPClient object at the IP and port no.---
+                TcpClient client = new TcpClient(ip_Adress, PORT_NO);
+                NetworkStream nwStream = client.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+                //---send the text---
+                Console.WriteLine("Sending : " + textToSend);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                client.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
